4ca94d66437c499842e6dca0c252480cb52b8693
zhao993059 <zhao993059@utexas.edu>
2025-04-09 10:38:10 -0500

    Added volume_of_sphere.py

---
+++ volume_of_sphere.py
@@ -0,0 +1,7 @@
+from math import pi
+
+def calculate_volume_of_sphere(radius):
+    return (4 / 3) * pi * radius ** 3
+
+print(f"Volume of sphere with radius 30: {calculate_volume_of_sphere(30)}")
+print(f"Volume of sphere with radius 40: {calculate_volume_of_sphere(40)}")
